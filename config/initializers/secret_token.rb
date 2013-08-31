c97ff1cf3725955994e603134ea240fec530d0bc
Edgard R. Navarrete <kinglouis.belmont@gmail.com>
2013-08-30 23:34:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStoreDos::Application.config.secret_token = '3a11063a80ffbaa63c9a2b9750cdce716babad8bcb0692694706072883f03ee5b8635a4a73a4a20d14ad47d6204ce323a887026a259a66ac57d89ffc6054882d'
